ddccb9245d17575fbafdba33592e8b2757cc4b96
refoilmiya <refo.ilmiya@gmail.com>
2020-03-13 22:35:40 +0700

    added wikidata integration

--- main.py
+++ main.py
@@ -1,28 +1,19 @@
 #!venv/bin/python3
 from flask import Flask, jsonify
+from wikidata import get_results
 
 app = Flask(__name__)
 
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web',
-        'done': False
-    }
-]
 
-
-@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+@app.route('/', methods=['GET'])
 def get_tasks():
-    return jsonify({'tasks': tasks})
+    endpoint_url = "https://query.wikidata.org/sparql"
+
+    query = "select ?item {?item  wdt:P31 wd:Q808.}"
+    results = get_results(endpoint_url, query)
+
+    return jsonify(results)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
---
+++ wikidata.py
@@ -0,0 +1,10 @@
+import sys
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+
+def get_results(endpoint_url, query):
+    user_agent = "WDQS-example Python/%s.%s" % (sys.version_info[0], sys.version_info[1])
+    sparql = SPARQLWrapper(endpoint_url, agent=user_agent)
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+    return sparql.query().convert()
