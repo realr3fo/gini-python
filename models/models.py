edf0501b52b45f397e0704c7b3f8b0daf6915281
refoilmiya <refo.ilmiya@gmail.com>
2020-04-18 18:39:56 +0700

    updated models

--- models/models.py
+++ models/models.py
@@ -24,3 +24,39 @@ class Logs(db.Model):
             'properties': self.properties,
             'timestamp': self.timestamp
         }
+
+
+class Dashboards(db.Model):
+    __tablename__ = 'dashboards'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    author = db.Column(db.String())
+    entity = db.Column(db.String())
+    hash_code = db.Column(db.String())
+    filters = db.Column(db.String())
+    properties = db.Column(db.String())
+    timestamp = db.Column(db.String())
+
+    def __init__(self, name, author, entity, hash_code, filters, properties, timestamp):
+        self.name = name
+        self.author = author
+        self.entity = entity
+        self.hash_code = hash_code
+        self.filters = filters
+        self.properties = properties
+        self.timestamp = timestamp
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'author': self.author,
+            'entity': self.entity,
+            'hash_code': self.hash_code,
+            'filters': self.filters,
+            'properties': self.properties,
+            'timestamp': self.timestamp
+        }
