fa5da4932b684c8540a72496bf085e54552d0ce5
refoilmiya <refo.ilmiya@gmail.com>
2020-04-28 16:03:05 +0700

    finished implementing entity count

--- resolver/resolve_card.py
+++ resolver/resolve_card.py
@@ -4,10 +4,15 @@ from bs4 import BeautifulSoup
 
 def resolve_get_entities_count_result(single_dashboard):
     entity = single_dashboard.entity
-    count = 0
     wikidata_url = "https://www.wikidata.org/w/index.php?search=haswbstatement%%3AP31%%3D%s&title=Special%%3ASearch" \
                    % entity
     wikidata_result = requests.get(wikidata_url)
     soup = BeautifulSoup(wikidata_result.text, "html.parser")
+    strong_elem = soup.findAll('strong')
+    entity_count = strong_elem[-1]
+    entity_count_text = entity_count.text
+    entity_count_text = ''.join(entity_count_text.split(","))
+    entity_count_int = int(entity_count_text)
+    count = entity_count_int
     result = {"entityCount": count}
     return result
